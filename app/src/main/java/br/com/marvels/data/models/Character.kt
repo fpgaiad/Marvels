5678fa685c222cfcffbb13817dba4d7f6737c686
Felipe Gaiad <fpgaiad@gmail.com>
2020-07-28 18:06:36 -0300

    Refactor Models

--- app/src/main/java/br/com/marvels/data/models/Character.kt
+++ app/src/main/java/br/com/marvels/data/models/Character.kt
@@ -10,17 +10,14 @@ import kotlinx.android.parcel.Parcelize
 @Entity(tableName = "characters")
 @Parcelize
 data class Character(
+
     @PrimaryKey
     val id: Int,
-    @Embedded(prefix = "comics_")
-    val comics: Comics,
     val description: String,
     val modified: String,
     val name: String,
     @ColumnInfo(name = "resource_uri")
     val resourceURI: String,
-    @Embedded(prefix = "series_")
-    val series: Series,
     @Embedded(prefix = "thumbnail_")
     val thumbnail: Thumbnail
 ) : Parcelable
